7cfea8b6adc9d6077e929129df44267f528f812d
NewtonKamau <kamaunewton78@gmail.com>
2023-05-13 13:04:51 +0300

    useState to get city input and fetchFunction() for that city

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
 import { Inter } from 'next/font/google'
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home () {
+  const [inputVal, setInputVal] = useState("");
+  const changeHandler = (e) => {
+    setInputVal(e.target.value)
+  }
+  const fetchFunction = async (inputVal) => {
+    try {
+      const {data} = await axios.get(`/api/weatherAPI?city=${inputVal}`);
+      //deconstruct the value from data 
+      const { weatherData } = data;
+      console.log("data:",weatherData)
+    } catch (error) {
+      console.log("Error occurred when fetching weather data ",error.message)
+    }
+  }
   return (
    <div className="bg-blue-300 mx-20 mt-20">
       <div className="flex justify-center items-center m-4">
@@ -12,9 +28,10 @@ export default function Home() {
           id="search-bar-id"
           type="search"
           placeholder="Search city or zip code..."
+          onChange={changeHandler}
         />
         
-        <button className="m-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out">
+        <button onClick={()=>fetchFunction(inputVal)} className="m-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out">
           Search 
         </button>
       </div>
