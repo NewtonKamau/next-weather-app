b4b9a94b7d6eee7118037335f6e096cb85df15fb
NewtonKamau <kamaunewton78@gmail.com>
2023-05-13 13:04:19 +0300

    get city dynamically

--- pages/api/weatherAPI.js
+++ pages/api/weatherAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const weather_api = process.env.WEATHER_API;
 export default async (req, res) => {
     try {
-        let city ='nairobi'
+        let city = req.query.city;
         const weather = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${weather_api}&q=${city}&day=10`);
         const data = weather.data;
         return res.status(200).json({ message: "sent successfully",weatherData:data })
