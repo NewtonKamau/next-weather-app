a178c9bc750a83c8e0a260efadd76aafbedd64ed
NewtonKamau <kamaunewton78@gmail.com>
2023-05-13 12:40:08 +0300

    weather api and axios

---
+++ pages/api/weatherAPI.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const weather_api = process.env.WEATHER_API;
+export default async (req, res) => {
+    try {
+        let city ='nairobi'
+        const weather = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${weather_api}&q=${city}&day=10`);
+        const data = weather.data;
+        return res.status(200).json({ message: "sent successfully",weatherData:data })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "there was an error fetching weather data" })
+    }
+};
